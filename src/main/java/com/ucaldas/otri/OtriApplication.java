64c3de6a288aef735a97bd1f4f136ab7bb7e42a9
Ramdhei <ramdhei.arcilaa@opitech.com.co>
2025-04-19 11:33:24 -0500

    initial commit

---
+++ src/main/java/com/ucaldas/otri/OtriApplication.java
@@ -0,0 +1,13 @@
+package com.ucaldas.otri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucaldas/otri/OtriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucaldas.otri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
